2fcab1fdf3ae9fe302bad3544ec4b4d12b558d75
reddytharun <reddytharun367@gmail.com>
2024-10-08 14:53:10 +0530

    nothingto tell

--- src/main/java/com/example/dockerspringboot/DockerspringbootApplication.java
+++ src/main/java/com/example/dockerspringboot/DockerspringbootApplication.java
@@ -9,7 +9,7 @@ public class DockerspringbootApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(DockerspringbootApplication.class, args);
-		System.out.println("nothing too");
+		System.out.println("nothing toosoomuch");
 	}
 
 }
