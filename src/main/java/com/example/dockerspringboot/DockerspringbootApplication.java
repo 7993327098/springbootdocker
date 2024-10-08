315bd785811d43646eb66fc9258683883f03af72
reddytharun <reddytharun367@gmail.com>
2024-10-08 14:44:53 +0530

    toshow

--- src/main/java/com/example/dockerspringboot/DockerspringbootApplication.java
+++ src/main/java/com/example/dockerspringboot/DockerspringbootApplication.java
@@ -9,7 +9,7 @@ public class DockerspringbootApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(DockerspringbootApplication.class, args);
-		System.out.println("nothing");
+		System.out.println("nothing too");
 	}
 
 }
